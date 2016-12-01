bcbea7ac770e5b62d14b9f555505143cf3fa8597
Michael Gates <michaelgatesdev@gmail.com>
2016-11-30 22:33:23 -0500

    Added villager prototype

---
+++ src/main/java/com/michaelgatesdev/OldLeaf/game/villager/Villager.java
@@ -0,0 +1,44 @@
+package com.michaelgatesdev.OldLeaf.game.villager;
+
+import com.michaelgatesdev.OldLeaf.game.player.appearance.Gender;
+
+public final class Villager
+{
+    private String          name;
+    private Species         species;
+    private Gender          gender;
+    private PersonalityType personality;
+
+
+    public Villager(String name, Species species, Gender gender, PersonalityType personality)
+    {
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.personality = personality;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+    public Species getSpecies()
+    {
+        return species;
+    }
+
+
+    public Gender getGender()
+    {
+        return gender;
+    }
+
+
+    public PersonalityType getPersonality()
+    {
+        return personality;
+    }
+}
