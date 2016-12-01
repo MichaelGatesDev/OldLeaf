881c76222c4c10d95205a6a723adf129b4e5cb3d
Michael Gates <michaelgatesdev@gmail.com>
2016-11-30 22:12:06 -0500

    Begin villager personality types

---
+++ src/main/java/com/michaelgatesdev/OldLeaf/game/villager/PersonalityType.java
@@ -0,0 +1,13 @@
+package com.michaelgatesdev.OldLeaf.game.villager;
+
+public enum PersonalityType
+{
+    LAZY,
+    JOCK,
+    CRANKY,
+    SMUG,
+    NORMAL,
+    PEPPY,
+    SNOOTY,
+    UCHI
+}
