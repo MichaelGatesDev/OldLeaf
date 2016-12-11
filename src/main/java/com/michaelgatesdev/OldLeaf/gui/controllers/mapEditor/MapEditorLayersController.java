b3e57f1e8a5380533ac93dc9a64a47f908c299f3
Michael Gates <michaelgatesdev@gmail.com>
2016-12-11 16:24:48 -0500

    Changed layers panel design

--- src/main/java/com/michaelgatesdev/OldLeaf/gui/controllers/mapEditor/MapEditorLayersController.java
+++ src/main/java/com/michaelgatesdev/OldLeaf/gui/controllers/mapEditor/MapEditorLayersController.java
@@ -1,6 +1,8 @@
 package com.michaelgatesdev.OldLeaf.gui.controllers.mapEditor;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import org.apache.log4j.Logger;
 
 import java.net.URL;
@@ -12,8 +14,15 @@ public class MapEditorLayersController implements Initializable
     
     private static final Logger logger = Logger.getLogger(MapEditorLayersController.class);
     
+    @FXML
+    public Label coordinatesLabel;
+    
+    @FXML
+    public Label objectNameLabel;
+    
     
     // ============================================================================================================================================ \\
+    
     
     @Override
     public void initialize(URL location, ResourceBundle resources)
